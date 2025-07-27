32e8136046ccda717fec6169f7475bcb9907e6d7
realzedin <168987081+realzedin@users.noreply.github.com>
2025-07-27 23:00:11 +0300

    Create Patch.cpp

---
+++ Patch.cpp
@@ -0,0 +1,9 @@
+00 a0 e3 1e ff 2f e1 => return 0 => fonksiyon olumlu 
+
+01 a0 e3 1e ff 2f e1 => return 1 => fonksiyon olumlu
+
+00 20 70 47 => nop & succes ret 0 => fonksiyon olumlu & belirsiz
+
+1e ff 2f e1 => nop => no directly sub.
+
+return -1 =>> signal error (hata)
